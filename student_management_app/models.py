af6638dabfe968b4ded1a829b90cff240c9cef8c
VincentGichohi <vincentegichohi@gmail.com>
2022-05-24 21:26:33 +0300

    create the session and customuser models

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -3,3 +3,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class SessionYearModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    session_start_year = models.DateField()
+    session_end_year = models.DateField()
+    objects = models.Manager()
+    
+
+#Overriding the Default Django Auth User and adding the One More Field(user_type)
+class CustomUser(AbstractUser):
+    user_type_data = ((1, 'HOD'), (2, 'Staff'), (3, 'Student'))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
+
+    
