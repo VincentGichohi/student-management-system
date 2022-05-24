aa0ba09a53841ba16b2c90a3f051309cfb3462e7
VincentGichohi <vincentegichohi@gmail.com>
2022-05-24 22:28:42 +0300

    Add a feedback student model

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -104,3 +104,12 @@ class LeaveReportStaff(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
+
+class FeedBackStudent(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feedback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
