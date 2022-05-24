c71ad63cc1bdf89dc24ab2c03e3a6cb824c5d306
VincentGichohi <vincentegichohi@gmail.com>
2022-05-24 22:34:57 +0300

    Add a notifications model for staff

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -130,3 +130,11 @@ class NotificationStudent(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
+
+class NotificationStaffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    stafff_id = models.ForeignKey(Staffs, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
