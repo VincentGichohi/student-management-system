556f0d395c9e8bc73b4eab75235b202dc2d9876a
VincentGichohi <vincentegichohi@gmail.com>
2022-05-25 16:30:23 +0300

    add a dates attendance view

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -191,3 +191,30 @@ def staff_update_attendance(request):
         "session_years": session_years
     }
     return render(request, "staff_template/update_attendance_template.html", context)
+
+
+@csrf_exempt
+def get_attendance_dates(request):
+    
+
+    # Getting Values from Ajax POST 'Fetch Student'
+    subject_id = request.POST.get("subject")
+    session_year = request.POST.get("session_year_id")
+
+    # Students enroll to Course, Course has Subjects
+    # Getting all data from subject model based on subject_id
+    subject_model = Subjects.objects.get(id=subject_id)
+
+    session_model = SessionYearModel.objects.get(id=session_year)
+
+    # students = Students.objects.filter(course_id=subject_model.course_id, session_year_id=session_model)
+    attendance = Attendance.objects.filter(subject_id=subject_model, session_year_id=session_model)
+
+    # Only Passing Student Id and Student Name Only
+    list_data = &#91;]
+
+    for attendance_single in attendance:
+        data_small={"id":attendance_single.id, "attendance_date":str(attendance_single.attendance_date), "session_year_id":attendance_single.session_year_id.id}
+        list_data.append(data_small)
+
+    return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
