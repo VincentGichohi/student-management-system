cce9df2341e29d16c3986ac377327c11a767e165
VincentGichohi <vincentegichohi@gmail.com>
2022-05-25 09:37:43 +0300

    Add a studentviews file

---
+++ student_management_app/StudentViews.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage #To upload Profile Picture
+from django.urls import reverse
+import datetime #To parse Input DateTime into Python Date Time Field
+
+from student_management_app.models import CustomUser, Staffs, Courses, Subjects, Students, Attendance, AttendanceReport, LeaveReportStudent, FeedBackStudent, StudentResult
+
+def student_home(request):
+    student_obj = Students.objects.get(admin=request.user.id)
+    total_attendance = AttendanceReport.objects.filter(student_id=student_obj).count()
+    attendance_present = AttendanceReport.objects.filter(student_id=student_obj, status=True).count()
+    attendance_absent = AttendanceReport.objects.filter(student_id=student_obj, status=False).count()
+
+    course_obj = Courses.objects.get(id=student_obj.course_id.id)
+    total_subjects = Subjects.objects.filter(course_id=course_obj).count()
+
