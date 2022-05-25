309fbc0ded21d5cdc4f74bba2c0e530b53b5e487
VincentGichohi <vincentegichohi@gmail.com>
2022-05-25 16:20:47 +0300

    get students view

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -128,3 +128,27 @@ def staff_feedback_save(request):
         except:
             messages.error(request, "Failed to Send Feedback.")
             return redirect('staff_feedback')
+
+# WE don't need csrf_token when using Ajax
+@csrf_exempt
+def get_students(request):
+    # Getting Values from Ajax POST 'Fetch Student'
+    subject_id = request.POST.get("subject")
+    session_year = request.POST.get("session_year")
+
+    # Students enroll to Course, Course has Subjects
+    # Getting all data from subject model based on subject_id
+    subject_model = Subjects.objects.get(id=subject_id)
+
+    session_model = SessionYearModel.objects.get(id=session_year)
+
+    students = Students.objects.filter(course_id=subject_model.course_id, session_year_id=session_model)
+
+    # Only Passing Student Id and Student Name Only
+    list_data = []
+
+    for student in students:
+        data_small={"id":student.admin.id, "name":student.admin.first_name+" "+student.admin.last_name}
+        list_data.append(data_small)
+
+    return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
