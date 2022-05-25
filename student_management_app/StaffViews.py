6beb3d434585d4e4c47577e90a2a7e201a17b574
VincentGichohi <vincentegichohi@gmail.com>
2022-05-25 19:30:19 +0300

    Add an attendance view

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -218,3 +218,19 @@ def get_attendance_dates(request):
         list_data.append(data_small)
 
     return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
+
+@csrf_exempt
+def get_attendance_student(request):
+    # Getting Values from Ajax POST 'Fetch Student'
+    attendance_date = request.POST.get('attendance_date')
+    attendance = Attendance.objects.get(id=attendance_date)
+
+    attendance_data = AttendanceReport.objects.filter(attendance_id=attendance)
+    # Only Passing Student Id and Student Name Only
+    list_data = &#91;]
+
+    for student in attendance_data:
+        data_small={"id":student.student_id.admin.id, "name":student.student_id.admin.first_name+" "+student.student_id.admin.last_name, "status":student.status}
+        list_data.append(data_small)
+
+    return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
