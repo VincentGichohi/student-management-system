f1af6a93d2904fe7ff44b89156128c41b48549e5
VincentGichohi <vincentegichohi@gmail.com>
2022-05-25 16:29:14 +0300

    Add a staff attendance view

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -152,3 +152,42 @@ def get_students(request):
         list_data.append(data_small)
 
     return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
+
+@csrf_exempt
+def save_attendance_data(request):
+    # Get Values from Staf Take Attendance form via AJAX (JavaScript)
+    # Use getlist to access HTML Array/List Input Data
+    student_ids = request.POST.get("student_ids")
+    subject_id = request.POST.get("subject_id")
+    attendance_date = request.POST.get("attendance_date")
+    session_year_id = request.POST.get("session_year_id")
+
+    subject_model = Subjects.objects.get(id=subject_id)
+    session_year_model = SessionYearModel.objects.get(id=session_year_id)
+
+    json_student = json.loads(student_ids)
+    # print(dict_student&#91;0]&#91;'id'])
+
+    # print(student_ids)
+    try:
+        # First Attendance Data is Saved on Attendance Model
+        attendance = Attendance(subject_id=subject_model, attendance_date=attendance_date, session_year_id=session_year_model)
+        attendance.save()
+
+        for stud in json_student:
+            # Attendance of Individual Student saved on AttendanceReport Model
+            student = Students.objects.get(admin=student.id)
+            attendance_report = AttendanceReport(student_id=student, attendance_id=attendance, status='status')
+            attendance_report.save()
+        return HttpResponse("OK")
+    except:
+        return HttpResponse("Error")
+
+def staff_update_attendance(request):
+    subjects = Subjects.objects.filter(staff_id=request.user.id)
+    session_years = SessionYearModel.objects.all()
+    context = {
+        "subjects": subjects,
+        "session_years": session_years
+    }
+    return render(request, "staff_template/update_attendance_template.html", context)
