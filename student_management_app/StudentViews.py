de93d74254ab05a2c272eb7f4c0a5c23313edd10
VincentGichohi <vincentegichohi@gmail.com>
2022-05-25 15:44:07 +0300

    Add a save leave view

--- student_management_app/StudentViews.py
+++ student_management_app/StudentViews.py
@@ -97,3 +97,22 @@ def student_apply_leave(request):
         "leave_data": leave_data
     }
     return render(request, 'student_template/student_apply_leave.html', context)
+
+
+def student_apply_leave_save(request):
+    if request.method != "POST":
+        messages.error(request, "Invalid Method")
+        return redirect('student_apply_leave')
+    else:
+        leave_date = request.POST.get('leave_date')
+        leave_message = request.POST.get('leave_message')
+ 
+        student_obj = Students.objects.get(admin=request.user.id)
+        try:
+            leave_report = LeaveReportStudent(student_id=student_obj, leave_date=leave_date, leave_message=leave_message, leave_status=0)
+            leave_report.save()
+            messages.success(request, "Applied for Leave.")
+            return redirect('student_apply_leave')
+        except:
+            messages.error(request, "Failed to Apply Leave")
+            return redirect('student_apply_leave')
