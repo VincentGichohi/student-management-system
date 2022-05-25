8deed6d88acb9eb281b8b0d420f3361379e8daf5
VincentGichohi <vincentegichohi@gmail.com>
2022-05-25 19:31:13 +0300

    update the attendance

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -234,3 +234,27 @@ def get_attendance_student(request):
         list_data.append(data_small)
 
     return JsonResponse(json.dumps(list_data), content_type="application/json", safe=False)
+
+
+@csrf_exempt
+def update_attendance_data(request):
+    student_ids = request.POST.get("student_ids")
+
+    attendance_date = request.POST.get("attendance_date")
+    attendance = Attendance.objects.get(id=attendance_date)
+
+    json_student = json.loads(student_ids)
+
+    try:
+        
+        for stud in json_student:
+            # Attendance of Individual Student saved on AttendanceReport Model
+            student = Students.objects.get(admin=stud&#91;'id'])
+
+            attendance_report = AttendanceReport.objects.get(student_id=student, attendance_id=attendance)
+            attendance_report.status=stud&#91;'status']
+
+            attendance_report.save()
+        return HttpResponse("OK")
+    except:
+        return HttpResponse("Error")
